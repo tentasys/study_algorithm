839b737cab64f1b920e6dd43f788f92dbf1d396b
Linkaden <tentasys@gmail.com>
2018-02-06 23:44:06 +0900

    commit

--- 9095/src/Main.java
+++ 9095/src/Main.java
@@ -5,23 +5,23 @@ import java.io.InputStreamReader;
 public class Main {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine().trim());
+		int arr[] = new int[10];
+		int input[] = new int[n+1];
 		
-		long [] arr = new long[n];
+		for(int i=1; i<n+1; i++)
+			input[i] = Integer.parseInt(br.readLine().trim());
 		
-		if(n > 1)
-			arr[1] = 1;
 		arr[0] = 1;
+		arr[1] = 2;
+		arr[2] = 4;
 		
-		for(int i=2; i<n; i++)
-			arr[i] = arr[i-1] + arr[i-2];
+		for(int i=3; i<10; i++)
+			arr[i] = arr[i-1] + arr[i-2] + arr[i-3];
 		
-		if(n==1)
-			System.out.println(1);
-		else
-			System.out.println(arr[n-1]);
+		for(int i=1; i<n+1; i++)
+			System.out.println(arr[input[i]-1]);
 	}
 
 }
--- N11726/src/Main.java
+++ N11726/src/Main.java
@@ -5,23 +5,19 @@ import java.io.InputStreamReader;
 public class Main {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine().trim());
 		
 		long [] arr = new long[n];
 		
 		if(n > 1)
-			arr[1] = 1;
+			arr[1] = 2;
 		arr[0] = 1;
 		
 		for(int i=2; i<n; i++)
 			arr[i] = arr[i-1] + arr[i-2];
 		
-		if(n==1)
-			System.out.println(1);
-		else
-			System.out.println(arr[n-1]);
+		System.out.println((arr[n-1])%10007);
 	}
 
 }
