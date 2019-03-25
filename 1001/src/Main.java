a4bc075e6a3a1f8ca7a47b18da37a7713bedf992
Linkaden <tentasys@gmail.com>
2019-03-25 20:47:13 +0900

    A-B

---
+++ 1001/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+	static int a, b;
+	public static void main(String[] args) {
+		try {
+			a = System.in.read()-'0';
+			System.in.read();
+			b = System.in.read()-'0';
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		System.out.print(a-b);
+	}
+}
