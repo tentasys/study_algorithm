02fef63f4ae09f2724fca784fe1f2bbaaa1b0729
Linkaden <tentasys@gmail.com>
2019-04-11 15:56:51 +0900

    gitignore및 테스트 수정

--- test/src/test/Main.java
+++ test/src/test/Main.java
@@ -1,39 +1,28 @@
 package test;
-
+import java.util.*;
+import java.io.*;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 
-		int UP[][] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+		BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+		String line = br.readLine();
+		StringTokenizer st = new StringTokenizer(line);
 		
-		for(int i=0; i<3; i++)
+		while(line != null)
 		{
-			for(int j=0; j<3; j++)
-				System.out.print(UP[i][j]+" ");
+			
+			st = new StringTokenizer(line);
+			while(st.hasMoreTokens())
+			{
+				System.out.print(Integer.parseInt(st.nextToken()) + " ");
+			}
+			
 			System.out.println();
+			line = br.readLine();
 		}
 		
-		//윗면 모든 요소가 시계방향으로 회전
-		int temp = UP[0][0];
-		//좌 3줄
-		UP[0][0] = UP[1][0];	UP[1][0] = UP[2][0];	UP[2][0] = UP[2][1];
-		//하 2줄
-		UP[2][1] = UP[2][2];	UP[2][2] = UP[1][2];
-		//우 2줄
-		UP[1][2] = UP[0][2];	UP[0][2] = UP[0][1];
-		//상 1줄
-		UP[0][1] = temp;		
-		
-		System.out.println();
-		for(int i=0; i<3; i++)
-		{
-			for(int j=0; j<3; j++)
-				System.out.print(UP[i][j]+" ");
-			System.out.println();
-		}
-	}
-
-
-	
+		br.close();
+	}	
 }
