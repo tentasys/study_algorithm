a731fea0e06a2aa73244d0872739c721d51b8adc
Linkaden <tentasys@gmail.com>
2019-04-25 21:47:03 +0900

    아스키 코드

---
+++ 11654/src/Main.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print((int)br.readLine().charAt(0));
+		br.close();
+	}
+
+}
