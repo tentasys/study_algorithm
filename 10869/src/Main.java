ea1899e0986d7792c5a57b5c90eac2989285145c
Linkaden <tentasys@gmail.com>
2019-06-19 23:07:46 +0900

    사칙연산

---
+++ 10869/src/Main.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append((a+b)+"\n");
+		sb.append((a-b)+"\n");
+		sb.append((a*b)+"\n");
+		sb.append((a/b)+"\n");
+		sb.append(a%b);
+		
+		System.out.print(sb);
+		
+		br.close();
+	}
+}
