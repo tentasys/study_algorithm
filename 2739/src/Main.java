e87198d4ae8251be92a600a10745e9d74b06aca8
Linkaden <tentasys@gmail.com>
2019-06-19 23:00:55 +0900

    구구단 출력

---
+++ 2739/src/Main.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i=1; i<=9; i++)
+		{
+			sb.append(num + " * " + i + " = " + (num*i) + "\n");
+		}
+		
+		System.out.print(sb);
+		
+		br.close();
+	}
+
+}
