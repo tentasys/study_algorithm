c1fcc1ba0a2f090c784de339aafd8e02a4e1a3c4
Linkaden <tentasys@gmail.com>
2019-06-19 22:54:58 +0900

    a*b(system.in.read 사용)

---
+++ 10998/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		int a = System.in.read() - '0';
+		System.in.read();
+		int b = System.in.read() - '0';
+		
+		System.out.print(a*b);
+	}
+}
