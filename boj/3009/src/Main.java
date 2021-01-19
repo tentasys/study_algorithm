b8a3f5526f1cb983335f17c63bd74f64e7994c4b
HeeJung Kang <tentasys@gmail.com>
2021-01-19 23:39:54 +0900

    3009 네 개의 점 : 진행중

---
+++ boj/3009/src/Main.java
@@ -0,0 +1,27 @@
+//자고 내일 할래
+https://www.acmicpc.net/problem/3009
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int x1 = -1;	int x2 = -1;
+		int y1 = -1;	int y2 = -1;
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		x1 = Integer.parseInt(st.nextToken());
+		y1 = Integer.parseInt(st.nextToken());
+		
+		
+		int x = 0;
+		int y = 0;
+		for(int i=0; i<3; i++) {
+			
+		}
+	}
+
+}
