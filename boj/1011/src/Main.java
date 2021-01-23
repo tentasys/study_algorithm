999767a7b9c9580a2c05398ebe5c1503548b29ec
HeeJung Kang <tentasys@gmail.com>
2021-01-23 19:28:21 +0900

    1011 Fly me to the Alpha Centauri : 수학 규칙, 정수의 최대 수를 넘어가는 범위에서의 연산

--- boj/1011/src/Main.java
+++ boj/1011/src/Main.java
@@ -12,9 +12,9 @@ public class Main {
 			int x = Integer.parseInt(st.nextToken());
 			int y = Integer.parseInt(st.nextToken());
 			
-			int sub = y-x;	//목표 거리 
+			long sub = y-x;	//목표 거리 
 			int n = 0;		//이동 횟수 n 
-			int dist = 0;	//n번 이동했을 때 갈 수 있는 최대 거리 
+			long dist = 0;	//n번 이동했을 때 갈 수 있는 최대 거리 
 			
 			//목표 거리가 n번 이동했을 때 갈 수 있는 최대 거리에 해당하는 시점을 찾으면 된다 
 			while(dist < sub) {
@@ -30,6 +30,10 @@ public class Main {
 					dist = ((n+2)/2)*((n+2)/2) - ((n+2)/2);
 				}
 				
+				//이 조건을 걸어놓지 않으면 오버플로우가 나서 음수에서 무한루프가 돈다 
+			    //1 2147483647 일 때 오류가 났었 
+				if(dist < 0)
+					break;
 			}
 			
 			System.out.println(n);
