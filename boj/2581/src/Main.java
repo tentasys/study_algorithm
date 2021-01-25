4477ff2ecfa5e837ff683201cafe95d618376b54
HeeJung Kang <tentasys@gmail.com>
2021-01-25 23:55:16 +0900

    2581 진행중

---
+++ boj/2581/src/Main.java
@@ -0,0 +1,40 @@
+import java.io.*;
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int M = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
+		
+		int sum = 0;
+		
+		for(int i=M; i<=N; i++) {
+			if(isPrime(i) == true) {
+				sum += i;
+				//System.out.println(i);
+			}
+				
+		}
+		
+		if(sum == 0)
+			System.out.println(-1);
+		else
+			System.out.println(sum);
+	}
+
+	public static boolean isPrime(int num) {
+		
+		if(num == 1)
+			return false;
+		else if(num == 2)
+			return true;
+		else {
+			for(int i=2; i<= Math.sqrt(num); i++) {
+				if(num%i == 0)
+					return false;
+			}
+		}
+		
+		return true;
+	}
+}
