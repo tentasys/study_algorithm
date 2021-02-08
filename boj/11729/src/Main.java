5ab33ba324857a2d001d3522b98e9d59584d24cf
HeeJung Kang <tentasys@gmail.com>
2021-02-08 22:47:13 +0900

    11729 하노이 탑 이동 순서 : 하노이 순서, 이동횟수

--- boj/11729/src/Main.java
+++ boj/11729/src/Main.java
@@ -1,23 +1,31 @@
+//참고 : https://shoark7.github.io/programming/algorithm/tower-of-hanoi
 import java.io.*;
 public class Main {
-
+	public static int count = 0;
+	public static StringBuilder sb = new StringBuilder();
+	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
+		
+		sb.append((int)Math.pow(2, N)-1+"\n");
 		hanoi(N, 1, 3, 2);
+		
+		System.out.println(sb);
 	}
 	public static void hanoi(int n, int from, int dest, int via) {
 		if(n == 1) {
-			move(n, from, dest);
+			move(n, from, dest);		
 		}
 		else {
 			hanoi(n-1, from, via, dest);
 			move(n, from, dest);
-			hanoi(n-1, via, from, dest);
+			hanoi(n-1, via, dest, from);
 		}
 	}
 	
 	public static void move(int n, int from, int dest) {
-		System.out.println(n+"번째 원반 : "+from+"->"+dest);
+		String str = from+" "+dest;
+		sb.append(str+"\n");
 	}
 }
