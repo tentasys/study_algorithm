e71f83a5d1ffd1b2fa76b3e64c0ec7a9fee4768b
HeeJung Kang <tentasys@gmail.com>
2021-02-10 23:19:39 +0900

    7568 덩치 : 브루트 포스

--- boj/7568/src/Main.java
+++ boj/7568/src/Main.java
@@ -5,64 +5,27 @@ public class Main {
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		Person arr[] = new Person[N];
+		int person[][] = new int[N][2];
 		
 		for(int i=0; i<N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int x = Integer.parseInt(st.nextToken());
-			int y = Integer.parseInt(st.nextToken());
-			arr[i] = new Person(x, y);
+			person[i][0] = Integer.parseInt(st.nextToken());
+			person[i][1] = Integer.parseInt(st.nextToken());
 		}
 		
-		int count = 0;	//등수 정보 셀 배열 
-		int sub_count = 0;	//그 등수 정보가 몇명인지 
-		ArrayList<Person> list = new ArrayList<Person>();	//대표로 하나씩만 
-		int order_count[] = new int[51];
-		
-		//각 요소에 대해서, 키가 크지만 몸무게가 작으면 같은 등수 
+		int order[] = new int[N];
 		for(int i=0; i<N; i++) {
-			//등수 정보가 세팅되어있으면 건너뛰기 
-			if(arr[i].order != -1)		continue;
-			arr[i].order = count;
-			count++;
-			sub_count = 1;
-
-			list.add(arr[i]);
-		
-			for(int j=i; j<N; j++) {
-				if((arr[i].x >= arr[j].x && arr[i].y <= arr[j].y) || (arr[i].x <= arr[j].x && arr[i].y >= arr[j].y))
-				{
-					arr[j].order = count;
-					sub_count++;
-				}			
+			int count = 0;
+			for(int j=0; j<N; j++) {
+				if(i==j)	continue;
+				
+				if(person[i][0] < person[j][0] && person[i][1] < person[j][1])
+					count++;
 			}
-			order_count[count] = sub_count;
+			order[i] = count+1;
 		}
 		
-		Collections.sort(list);
-		int order[] = new int[51];
-		
-		order[list.get(0).order] = 1;
-		for(int i=1; i<count; i++) {
-			int cur = list.get(i).order;
-			int before = list.get(i-1).order;
-			order[cur] = order_count[before]+order[before]-1;
-		}
-		
-		for(int i=0; i<arr.length; i++) {
-			System.out.print(order[arr[i].order]+" ");
-		}
-	}
-}
-class Person implements Comparable<Person>{
-	int x;	//몸무게 
-	int y;	//키 
-	int order;	//등수 
-	Person(int x, int y){
-		this.x = x;	this.y = y;	this.order = -1;
-	}
-	@Override
-	public int compareTo(Person o) {
-		return o.x - this.x;
+		for(int i=0; i<N; i++)
+			System.out.print(order[i]+" ");
 	}
 }
