852cfd060a731a72a7c2882a3f8416e6d7110b7f
HeeJung Kang <tentasys@gmail.com>
2021-03-11 23:57:01 +0900

    15651 중복순열 : 속도 개

--- boj/15651/src/Main.java
+++ boj/15651/src/Main.java
@@ -1,34 +1,32 @@
 //중복순열 
 import java.io.*;
 
-public class Main {
-
-	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+public class Main { 
+	static StringBuilder result = new StringBuilder();
+	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String str[] = br.readLine().split(" ");
 		int N = Integer.parseInt(str[0]);
 		int M = Integer.parseInt(str[1]);
 		
-		f(N, M, 0, new int[M]);
-		
+		f(N, M, 0);
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write(result.toString());
 		bw.flush();
 	}
 
-	public static void f(int N, int M, int count, int[] arr) throws Exception{
+	static StringBuilder sb = new StringBuilder();
+	public static void f(int N, int M, int count) throws Exception{
 		if(count == M) {
-			StringBuilder sb = new StringBuilder();
-			
-			for(int a : arr) {
-				sb.append(a+" ");
-			}
-			
-			bw.write(sb.toString()+"\n");
+			result.append(sb+"\n");
 			return;
 		}
 		for(int i=1; i<=N; i++) {
-			arr[count] = i;
-			f(N, M, count+1, arr);
+			sb.append(i+" ");
+			f(N, M, count+1);
+			sb.deleteCharAt(sb.length()-1);
+			sb.deleteCharAt(sb.length()-1);
 		}
 	}
 }
