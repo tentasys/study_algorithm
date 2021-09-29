8ac95af9faeee666a6b79aec7cc64d1137d81ab2
HeeJung Kang <tentasys@gmail.com>
2021-09-29 22:02:12 +0900

    1541 잃어버린 괄호 : 그리디

--- boj/1541/src/Main.java
+++ boj/1541/src/Main.java
@@ -6,8 +6,9 @@ public class Main {
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		char arr[] = br.readLine().toCharArray();
-		ArrayList<Integer> list = new ArrayList<Integer>();
 		
+		//입력받은 숫자를 Integer형으로 파싱 
+		ArrayList<Integer> list = new ArrayList<Integer>();
 		StringBuilder sb = new StringBuilder();
 		
 		for(int i=0; i<arr.length; i++) {
@@ -24,11 +25,13 @@ public class Main {
 		list.add(Integer.parseInt(sb.toString()));
 		
 		br.close();
+		//파싱 끝 
 		
+		//마이너스 -> 무조건 뺀다 
+		//플러스 -> 처음만 더하고, 그 뒤에 나오는 플러스는 다른 마이너스 만날때까지 더하다가 뺌 
 		int result = 0;
 		int sum = 0;
 		
-
 		for(int a : list) {
 			if(a >= 0) {
 				if(sum < 0) {
