d4c2fb59ec9e08af20f604f0627ad96c18f0b862
HeeJung Kang <tentasys@gmail.com>
2021-10-07 22:16:43 +0900

    2658 십자카드 : 정렬

--- boj/2659/src/Main.java
+++ boj/2659/src/Main.java
@@ -1,33 +1,38 @@
+//1시간만의 삽질... 문제 이해 잘못함 
 import java.io.*;
-import java.util.*;
 public class Main {
 
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String str[] = br.readLine().split(" ");
+		
 		int arr[] = new int[4];
-		for(int i=0; i<4; i++)
-			arr[i] = Integer.parseInt(str[i]);
-		
-		Arrays.sort(arr);
-		
-		int cnt = 0;
-		
-		for(int i=1; i<10; i++) {
-			for(int j=1; j<10; j++) {				
-				if(i > j)	continue;			
-				for(int k=1; k<10; k++) {					
-					if(j > k)	continue;
-					for(int h=1; h<10; h++) {						
-						if(k > h)	continue;					
-						cnt++;
-						if(i==arr[0] && j==arr[1] && k==arr[2] && h==arr[3]) {
-							System.out.println(cnt);
-							return;
-						}
-					}
-				}
-			}
-		}
+	   	for(int i=0; i<4; i++)
+	   		arr[i] = Integer.parseInt(str[i]);
+	 
+	    int result = findclock(arr[0]*1000+arr[1]*100+arr[2]*10+arr[3]);
+	    int cnt = 0;
+	    for (int i = 1111; i <= result; i++)
+	    {
+	        if (findclock(i) == i) {
+	        	//System.out.println(i);
+	        	cnt++;
+	        }
+	    }
+	    System.out.println(cnt);
 	}
+
+	
+	static int findclock(int num){
+	    int result = num;
+	    int cal = num;
+	    //밀면서 탐색하기
+	    for (int i = 0; i < 3; i++)
+	    {
+	    	cal = cal % 1000 * 10 + cal / 1000;
+	    	result = Math.min(cal,  result);
+	    }
+	    return result;
+	}
+	 
 }
